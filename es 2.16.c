82ac7f9c094135658666f26943b4a312ddecde6b
daniele998 <danieledemarco998@gmail.com>
2015-12-16 18:26:38 +0100

    Update es 2.16.c

--- es 2.16.c
+++ es 2.16.c
@@ -21,11 +21,14 @@ int a,b,c;
 		
 		printf("MODULO\n");
 		c=a%b;
-		printf("Il modulo %d * %d e' uguale a %d\n", a, b, c);
+		printf("Il modulo %d % %d e' uguale a % %d\n", a, b, c);
+
+		printf("PRODOTTO\n");
+		c=a*b;
+		printf("Il modulo %d * %d e' uguale a % %d\n", a, b, c);
 
 		
 		
 
 return 0;
 }
-
