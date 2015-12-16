0493dd36b059294b2d6e3dec90b67bcd7f43317f
daniele998 <danieledemarco998@gmail.com>
2015-12-16 18:16:12 +0100

    Update es 2.16.c

--- es 2.16.c
+++ es 2.16.c
@@ -2,11 +2,12 @@
 int main(){
 int a,b,c;
 
-		printf("SOMMA\n");
 		printf("Inserisci il primo numero: ");	
 		scanf("%d",&a);
 		printf("Inserisci il secondo numero: ");	
 		scanf("%d",&b);
+
+		printf("SOMMA\n");
 		c=a+b;
 		printf("La somma %d + %d e' uguale a %d\n", a, b, c);
 
@@ -14,14 +15,17 @@ int a,b,c;
 		c=a-b;
 		printf("La differenza %d - %d e' uguale a %d\n", a, b, c);
 
-		printf("MODULO\n");
-		c=a*b;
-		printf("Il modulo %d * %d e' uguale a %d\n", a, b, c);
-
 		printf("QUOZIENTE\n");
 		c=a/b;
 		printf("Il quoziente %d / %d e' uguale a %d\n", a, b, c);
 		
+		printf("MODULO\n");
+		c=a%b;
+		printf("Il modulo %d * %d e' uguale a %d\n", a, b, c);
+
+		
+		
 
 return 0;
 }
+
